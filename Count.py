6c34e3a35979e6b8059ae93af0a917b58d270fec
Yixiong <christianding@me.com>
2018-04-16 11:26:47 +1000

    Upload

---
+++ Count.py
@@ -0,0 +1,42 @@
+import ngram
+import sys
+
+token_file = open("labelled-tokens.txt",'r')
+def main():
+    final_result= [0,0,0]
+    temp = [0,0,0]
+    one = 0
+    two = 0
+    three = 0
+    four = 0
+    five = 0
+    six = 0
+    seven = 0
+    eight = 0
+    nine = 0
+    for line in token_file:
+        line = line.split("\t")
+        token = line[0]
+        kind = line[1]
+        if(len(token)== 1):
+            one+=1
+        if(len(token)== 2):
+            two+=1
+        if(len(token)== 3):
+            three+=1
+        if(len(token)== 4):
+            four+=1
+        if(len(token)== 5):
+            five+=1
+        if(len(token)== 6):
+            six+=1
+        if(len(token)== 7):
+            seven+=1
+        if(len(token)== 8):
+            eight+=1
+
+    print(one,two,three,four,five,six,seven,eight)
+
+if __name__ == "__main__":
+    main()
+
---
+++ Global_Edit_Distance.py
@@ -0,0 +1,48 @@
+import editdistance
+import sys
+
+token_file = open("labelled-tokens.txt",'r')
+
+def check(token, o_token):
+
+    dict_file = open("dict.txt",'r')
+    best_val = sys.maxint
+    best_str = " "
+    result = [0,0,0]
+    result[0] += 1
+
+    for dict_line in dict_file:
+        val = editdistance.eval(token, dict_line.strip())
+        if val < best_val:
+            best_val = val
+            best_str = dict_line.strip()
+        if best_val == 0L:
+            break
+
+    if best_str == o_token:
+        #print(token,o_token,best_str)
+        result[1] += 1
+    else:
+        #print(token,o_token,best_str, val, best_val)
+        result[2] += 1
+    return result
+
+def main():
+    final_result= [0,0,0]
+    temp = [0,0,0]
+    for line in token_file:
+        line = line.split("\t")
+        token = line[0]
+        kind = line[1]
+        o_token = line[2].strip("\n")
+        if(kind == "OOV"):
+            temp = check(token,o_token)
+        final_result[0] += temp[0]
+        final_result[1] += temp[1]
+        final_result[2] += temp[2]
+
+    print('\nTotal: %d\nCorrect matches: %d\nIncorrect matches: %d\nAccuracy: %.2f%%\n' %(final_result[0],final_result[1],final_result[2],final_result[1]*100.0/final_result[0]));
+
+if __name__ == "__main__":
+    main()
+
